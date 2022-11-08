f51bdf79b5b6ecfc9db96307b4c0689caaabd2f2
James Oubre <oubrejames@gmail.com>
2022-11-08 14:59:11 -0600

    Plan working with more abstraction

--- moveBot/moveBot/intercept.py
+++ moveBot/moveBot/intercept.py
@@ -10,12 +10,11 @@ import geometry_msgs
 import octomap_msgs
 import sensor_msgs
 import trajectory_msgs
-from moveit_msgs.msg import PositionIKRequest, MotionPlanRequest
+from moveit_msgs.msg import PositionIKRequest, MotionPlanRequest, Constraints, JointConstraint, PositionConstraint
 from sensor_msgs.msg import JointState
 from moveit_msgs.srv import GetPositionIK
 from std_srvs.srv import Empty
 from rclpy.callback_groups import ReentrantCallbackGroup
-
 
 class Testing(Node):
     def __init__(self):
@@ -47,8 +46,6 @@ class Testing(Node):
          """
         self.joint_statesmsg=jointstate 
         # self.get_logger().info(f'goal msg {self.joint_statesmsg}')
-    
-
 
     def get_ik(self):
         ikmsg = PositionIKRequest()
@@ -59,14 +56,14 @@ class Testing(Node):
         ikmsg.pose_stamped.header.frame_id = 'panda_link0'
         ikmsg.pose_stamped.header.stamp = self.get_clock().now().to_msg()
         ikmsg.pose_stamped.pose.position.x = 0.5
-        ikmsg.pose_stamped.pose.position.y = 0.5
+        ikmsg.pose_stamped.pose.position.y = -0.5
         ikmsg.pose_stamped.pose.position.z = 0.5
         ikmsg.pose_stamped.pose.orientation.x = 0.0
         ikmsg.pose_stamped.pose.orientation.y = 0.0
         ikmsg.pose_stamped.pose.orientation.z = 0.0
         ikmsg.pose_stamped.pose.orientation.w = 0.0
         ikmsg.timeout.sec = 5
-
+        
         return ikmsg
         # self.ik_future = self.ik_client.call_async(GetPositionIK.Request(ik_request = ikmsg))
 
@@ -77,221 +74,70 @@ class Testing(Node):
  
         msg=self.get_ik()
         
-        self.response=await self.ik_client.call_async(GetPositionIK.Request(ik_request=msg))
+        self.ik_response=await self.ik_client.call_async(GetPositionIK.Request(ik_request=msg))
         # self.response=GetPositionIK.Response()
-        self.get_logger().info(f'\nresponse\n{self.response}')
-
+        self.get_logger().info(f'\nresponse\n{self.ik_response}')
+        #self.get_motion_request(self.ik_response)
         response=Empty.Response()
-        return response 
-
+        return response
     
+    def get_motion_request(self):
+        motion_req = MotionPlanRequest()
+        motion_req.workspace_parameters.header.stamp = self.get_clock().now().to_msg()
+        motion_req.workspace_parameters.min_corner.x = -1.0
+        motion_req.workspace_parameters.min_corner.y = -1.0
+        motion_req.workspace_parameters.min_corner.z = -1.0
+        motion_req.workspace_parameters.max_corner.x = 1.0
+        motion_req.workspace_parameters.max_corner.y = 1.0
+        motion_req.workspace_parameters.max_corner.z = 1.0
+        motion_req.workspace_parameters.header.frame_id = 'panda_link0'
+        motion_req.start_state.joint_state = self.joint_statesmsg
+        goal_constraints = Constraints()
+        
+        #joint_constraints = JointConstraint()
+        for i in range(len(self.joint_statesmsg.name)):
+            print("TEST\n",self.joint_statesmsg.name[i])
+            joint_constraints = JointConstraint()
+            joint_constraints.joint_name = self.joint_statesmsg.name[i]
+            joint_constraints.position = self.ik_response.solution.joint_state.position[i]
+            joint_constraints.tolerance_above = 0.0001
+            joint_constraints.tolerance_below = 0.0001
+            joint_constraints.weight = 1.0
+            goal_constraints.joint_constraints.append(joint_constraints)
+            #goal_constraints.joint_constraints[i] = joint_constraints
+        print("GOAL CONSTRAINTS\n", goal_constraints)
+        motion_req.goal_constraints = [goal_constraints]
+        motion_req.pipeline_id = 'move_group'
+        motion_req.group_name = 'panda_manipulator'
+        motion_req.num_planning_attempts = 10
+        motion_req.allowed_planning_time = 5.0
+        motion_req.max_velocity_scaling_factor = 0.1
+        motion_req.max_acceleration_scaling_factor = 0.1
+        motion_req.max_cartesian_speed = 0.0
+        
+        return motion_req
+        
+    
+        #mtn_reg.goal_constraints.joint_constraints = ik_resp. 
+        # joint_names = self.ik_resp.name
+        # joint_positions = self.ik_resp.position
+        
     def send_plan(self):
-        plan_request=moveit_msgs.action.MoveGroup.Goal(
-            request=moveit_msgs.msg.MotionPlanRequest(
-            workspace_parameters=moveit_msgs.msg.WorkspaceParameters(
-            header=std_msgs.msg.Header(
-            stamp=self.get_clock().now().to_msg(),
-            frame_id='panda_link0'),
-            min_corner=geometry_msgs.msg.Vector3(
-            x=-1.0,
-            y=-1.0,
-            z=-1.0),
-            max_corner=geometry_msgs.msg.Vector3(
-            x=1.0,
-            y=1.0,
-            z=1.0)),
-            start_state=moveit_msgs.msg.RobotState(
-            joint_state=sensor_msgs.msg.JointState(
-            header=std_msgs.msg.Header(
-            stamp=builtin_interfaces.msg.Time(
-            sec=0,
-            nanosec=0),
-            frame_id='panda_link0'),
-            name=['panda_joint1',
-            'panda_joint2',
-            'panda_joint3',
-            'panda_joint4',
-            'panda_joint5',
-            'panda_joint6',
-            'panda_joint7',
-            'panda_finger_joint1',
-            'panda_finger_joint2'],
-            position=[-3.05743667552222e-05,
-            0.45826129385071196,
-            0.36520248871411476,
-            -1.8763676740530548,
-            -0.21278170570578495,
-            2.2968262699790225,
-            1.2581421343073955,
-            0.035,
-            0.035],
-            velocity=[],
-            effort=[]),
-            multi_dof_joint_state=sensor_msgs.msg.MultiDOFJointState(
-            header=std_msgs.msg.Header(
-            stamp=builtin_interfaces.msg.Time(
-            sec=0,
-            nanosec=0),
-            frame_id='panda_link0'),
-            joint_names=[],
-            transforms=[],
-            twist=[],
-            wrench=[]),
-            attached_collision_objects=[],
-            is_diff=False),
-            goal_constraints=[moveit_msgs.msg.Constraints(
-            name='',
-            joint_constraints=[moveit_msgs.msg.JointConstraint(
-            joint_name='panda_joint1',
-            position=-0.799720847385826,
-            tolerance_above=0.0001,
-            tolerance_below=0.0001,
-            weight=1.0),
-            moveit_msgs.msg.JointConstraint(
-            joint_name='panda_joint2',
-            position=0.6631403268111388,
-            tolerance_above=0.0001,
-            tolerance_below=0.0001,
-            weight=1.0),
-            moveit_msgs.msg.JointConstraint(
-            joint_name='panda_joint3',
-            position=0.1428222590095236,
-            tolerance_above=0.0001,
-            tolerance_below=0.0001,
-            weight=1.0),
-            moveit_msgs.msg.JointConstraint(
-            joint_name='panda_joint4',
-            position=-1.9078958035320892,
-            tolerance_above=0.0001,
-            tolerance_below=0.0001,
-            weight=1.0),
-            moveit_msgs.msg.JointConstraint(
-            joint_name='panda_joint5',
-            position=-0.16023113114248438,
-            tolerance_above=0.0001,
-            tolerance_below=0.0001,
-            weight=1.0),
-            moveit_msgs.msg.JointConstraint(
-            joint_name='panda_joint6',
-            position=2.5601750273131985,
-            tolerance_above=0.0001,
-            tolerance_below=0.0001,
-            weight=1.0),
-            moveit_msgs.msg.JointConstraint(
-            joint_name='panda_joint7',
-            position=0.2327714355052132,
-            tolerance_above=0.0001,
-            tolerance_below=0.0001,
-            weight=1.0)],
-            position_constraints=[],
-            orientation_constraints=[],
-            visibility_constraints=[])],
-            path_constraints=moveit_msgs.msg.Constraints(
-            name='',
-            joint_constraints=[],
-            position_constraints=[],
-            orientation_constraints=[],
-            visibility_constraints=[]),
-            trajectory_constraints=moveit_msgs.msg.TrajectoryConstraints(
-            constraints=[]),
-            reference_trajectories=[],
-            pipeline_id='move_group',
-            planner_id='',
-            group_name='panda_manipulator',
-            num_planning_attempts=10,
-            allowed_planning_time=5.0,
-            max_velocity_scaling_factor=0.1,
-            max_acceleration_scaling_factor=0.1,
-            cartesian_speed_end_effector_link='',
-            max_cartesian_speed=0.0),
-            planning_options=moveit_msgs.msg.PlanningOptions(
-            planning_scene_diff=moveit_msgs.msg.PlanningScene(
-            name='',
-            robot_state=moveit_msgs.msg.RobotState(
-            joint_state=sensor_msgs.msg.JointState(
-            header=std_msgs.msg.Header(
-            stamp=builtin_interfaces.msg.Time(
-            sec=0,
-            nanosec=0),
-            frame_id=''),
-            name=[],
-            position=[],
-            velocity=[],
-            effort=[]),
-            multi_dof_joint_state=sensor_msgs.msg.MultiDOFJointState(
-            header=std_msgs.msg.Header(
-            stamp=builtin_interfaces.msg.Time(
-            sec=0,
-            nanosec=0),
-            frame_id=''),
-            joint_names=[],
-            transforms=[],
-            twist=[],
-            wrench=[]),
-            attached_collision_objects=[],
-            is_diff=True),
-            robot_model_name='',
-            fixed_frame_transforms=[],
-            allowed_collision_matrix=moveit_msgs.msg.AllowedCollisionMatrix(
-            entry_names=[],
-            entry_values=[],
-            default_entry_names=[],
-            default_entry_values=[]),
-            link_padding=[],
-            link_scale=[],
-            object_colors=[],
-            world=moveit_msgs.msg.PlanningSceneWorld(
-            collision_objects=[],
-            octomap=octomap_msgs.msg.OctomapWithPose(
-            header=std_msgs.msg.Header(
-            stamp=builtin_interfaces.msg.Time(
-            sec=0,
-            nanosec=0),
-            frame_id=''),
-            origin=geometry_msgs.msg.Pose(
-            position=geometry_msgs.msg.Point(
-            x=0.0,
-            y=0.0,
-            z=0.0),
-            orientation=geometry_msgs.msg.Quaternion(
-            x=0.0,
-            y=0.0,
-            z=0.0,
-            w=1.0)),
-            octomap=octomap_msgs.msg.Octomap(
-            header=std_msgs.msg.Header(
-            stamp=builtin_interfaces.msg.Time(
-            sec=0,
-            nanosec=0),
-            frame_id=''),
-            binary=False,
-            id='',
-            resolution=0.0,
-            data=[]))),
-            is_diff=True),
-            plan_only=True,
-            look_around=False,
-            look_around_attempts=0,
-            max_safe_execution_cost=0.0,
-            replan=False,
-            replan_attempts=0,
-            replan_delay=0.0))
-
+        plan_request=MoveGroup.Goal()
+        plan_request.request = self.get_motion_request()
+        plan_request.planning_options.plan_only = True
+        print("Plan request:\n")
+        print(plan_request)
         return plan_request
 
-
-
     async def plan_callback(self,request,response):
- 
         plan_msg=self.send_plan()
         self.future_response=await self._plan_client.send_goal_async(plan_msg)
         # self.response=GetPositionIK.Response()
-        self.response=await self.future_response.get_result_async()
-        self.get_logger().info(f'\nresponse\n{self.response}')
-
+        self.plan_response=await self.future_response.get_result_async()
+        self.get_logger().info(f'\nPlan rRsponse:\n{self.plan_response}')
         response=Empty.Response()
         return response 
-
 
     def send_execute(self):
         execute_msg=moveit_msgs.action.ExecuteTrajectory.Goal(
@@ -1334,15 +1180,15 @@ class Testing(Node):
 
         return execute_msg
 
-    
     async def execute_callback(self,request,response):
  
         exec_msg=self.send_execute()
         self.future_response=await self._execute_client.send_goal_async(exec_msg)
-        self.response=await self.future_response.get_result_async()
-        self.get_logger().info(f'\nresponse\n{self.response}')
+        self.execute_response=await self.future_response.get_result_async()
+        self.get_logger().info(f'\nresponse\n{self.execute_response}')
 
         response=Empty.Response()
+        
         return response 
 
 
