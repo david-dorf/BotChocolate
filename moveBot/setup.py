4b8c39e2d436f0d5de222cfefe66b43358c71862
Shantao Cao <caos1@rose-hulman.edu>
2022-11-08 19:59:44 -0600

    Add box example

--- moveBot/setup.py
+++ moveBot/setup.py
@@ -20,7 +20,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': ['intercept = moveBot.intercept:main',
-                            'moveBot = moveBot.moveBot:main'
+                            'moveBot = moveBot.moveBot:main',
+                            'brick_intercept = moveBot.brick_intercept:main'
         ],
     },
 )
