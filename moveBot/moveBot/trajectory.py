75751b6d530727a15de11650268b76d0030e20db
David <daviddorf2023@u.northwestern.edu>
2022-11-16 15:28:06 -0600

    Changed various aspects of request being sent by client. Still getting async tracemalloc errors.

--- moveBot/moveBot/trajectory.py
+++ moveBot/moveBot/trajectory.py
@@ -3,7 +3,7 @@ from rclpy.node import Node
 from enum import Enum, auto
 from rclpy.callback_groups import ReentrantCallbackGroup
 from std_srvs.srv import Empty
-from movebot_interfaces.srv import IkGoalRqst, AddBox
+from movebot_interfaces.srv import IkGoalRqst, AddBox, GetPlanRqst
 from movebot_interfaces.msg import IkGoalRqstMsg
 
 # class State(Enum):
@@ -16,22 +16,22 @@ class TrajectoryCaller(Node):
     def __init__(self):
         super().__init__("trajectory_node")
         self.cbgroup = ReentrantCallbackGroup()
-        self.plan_client = self.create_client(IkGoalRqst,"call_plan")
-        self.execute_client = self.create_client(Empty,"call_execute")
+        self.plan_client = self.create_client(GetPlanRqst,"call_plan",callback_group=self.cbgroup)
+        self.execute_client = self.create_client(Empty,"call_execute",callback_group=self.cbgroup)
+        self.request = GetPlanRqst.Request()
 
     async def send_request(self):
         """Build the desired IkGoalRqstMsg to be sent over the client to make the robot plan and
         execute a trajectory.
         """
-        start_pos = IkGoalRqstMsg()
-        goal_pos = IkGoalRqstMsg()
-        request = IkGoalRqst().Request(start_pos,goal_pos)
-        request.start_pos.position = []
-        request.start_pos.orientation = []
-        request.goal_pos.position = [0.5, 0.5, 0.5] # placeholder values, replace with CV
-        request.goal_pos.orientation = [0.0, 0.0, 0.0]
+        self.request.start_pos = IkGoalRqstMsg()
+        self.request.goal_pos = IkGoalRqstMsg()
+        self.request.start_pos.position = []
+        self.request.start_pos.orientation = []
+        self.request.goal_pos.position = [0.5, 0.5, 0.5] # placeholder values, replace with CV
+        self.request.goal_pos.orientation = [0.0, 0.0, 0.0]
         # find gripper flag in interface proto for GetPlanRqst
-        response = await self.plan_client.call_async(request)
+        response = await self.plan_client.call_async(self.request)
         return response
 
 
@@ -39,6 +39,7 @@ def main(args=None):
     rclpy.init(args=args)
     trajectory_client = TrajectoryCaller()
     trajectory_client.send_request()
+    # rclpy.spin(trajectory_client)
     trajectory_client.destroy_node()
     rclpy.shutdown()
 
