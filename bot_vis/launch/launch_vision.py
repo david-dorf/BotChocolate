44fbc50f05d9b0f0251b78fa818c53867717bc3b
James Oubre <oubrejames@gmail.com>
2022-11-20 15:29:34 -0600

    April tags all showing in rviz. Updated tag.yaml

--- bot_vis/launch/launch_vision.py
+++ bot_vis/launch/launch_vision.py
@@ -5,31 +5,44 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
 
 
 def generate_launch_description():
 
     bot_vis_path = get_package_share_path('bot_vis')
     tag_yaml_path = bot_vis_path / 'tag.yaml'
-    
+    default_rviz_config_path = bot_vis_path / 'urdf.rviz'
+
+    rviz_arg = DeclareLaunchArgument(
+    name='rvizconfig',
+    default_value=str(default_rviz_config_path),
+    description='Absolute path to rviz config file'
+    )
+
     realsense_node = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
             get_package_share_directory('realsense2_camera')),
             '/launch/rs_launch.py'])
     )
 
-    config = os.path.join(
-        get_package_share_path('bot_vis'),
-        'tag.yaml'
-    )
     # Start turtle robot node
     apriltag_node = Node(
         package='apriltag_ros',
         executable='apriltag_node',
         remappings=[('image_rect','/camera/color/image_raw'),
                     ('camera_info','/camera/color/camera_info')],
-        parameters=[config]
+        parameters=[tag_yaml_path]
         )
+    
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', LaunchConfiguration('rvizconfig')],
+    )
 
     return LaunchDescription([
         realsense_node,
