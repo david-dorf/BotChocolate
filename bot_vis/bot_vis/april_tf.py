7f693ea1cafe39f1de4359a6dd016d137e03924c
James Oubre <oubrejames@gmail.com>
2022-11-21 09:51:26 -0600

    Total TF tree connected

--- bot_vis/bot_vis/april_tf.py
+++ bot_vis/bot_vis/april_tf.py
@@ -16,7 +16,7 @@ from tf2_ros.transform_listener import TransformListener
 from geometry_msgs.msg import Point
 from geometry_msgs.msg import Pose
 from geometry_msgs.msg import TransformStamped
-
+from tf2_ros.static_transform_broadcaster import StaticTransformBroadcaster
 
 class AprilTF(Node):
     """
@@ -43,40 +43,65 @@ class AprilTF(Node):
         self.tf_listener = TransformListener(self.tf_buffer, self)
 
         # Create a broadcaster to link april tag TF's to robot arm TF's
-        self.ee_tag_2_panda_hand_tcp = TransformStamped()
-        self.ee_tag_2_panda_hand_tcp.header.stamp = self.get_clock().now().to_msg()
-        self.ee_tag_2_panda_hand_tcp.header.frame_id = "panda_hand_tcp"
-        self.ee_tag_2_panda_hand_tcp.child_frame_id = "end_eff_tag"
-        self.broadcaster = TransformBroadcaster(self)
+        # TODO i think when defining the actual positions of this transform, itll just be from the yaml
+        # Also TODO imort yaml (launch file)
+        self.world_2_panda_link0 = TransformStamped()
+        self.world_2_panda_link0.header.stamp = self.get_clock().now().to_msg()
+        self.world_2_panda_link0.header.frame_id = "world"
+        self.world_2_panda_link0.child_frame_id = "panda_link0"
+        self.broadcaster = StaticTransformBroadcaster(self)
+
+        # Create a broadcaster to link april tag TF's to robot arm TF's
+        self.world_2_cam = TransformStamped()
+        self.world_2_cam.header.stamp = self.get_clock().now().to_msg()
+        self.world_2_cam.header.frame_id = "world"
+        self.world_2_cam.child_frame_id = "camera_link"
+        self.broadcaster2 = StaticTransformBroadcaster(self)
+        
+        # # Create a broadcaster to link === to ===
+        # self.world2panda = TransformStamped()
+        # self.world2panda.header.stamp = self.get_clock().now().to_msg()
+        # self.world2panda.header.frame_id = "world"
+        # self.world2panda.child_frame_id = "panda_link0"
+        # self.broadcaster2 = TransformBroadcaster(self)
 
         self.timer = self.create_timer(1/100, self.timer_callback)
 
-    def calibrate(self):
-        """Obtain TF from camera to robot frame
-        """
-        
     def get_april_2_robot(self):
         """See if april tag is in in end eff. If so use its position to get the TF to base from
         april tag. If not, use saved TF
         """
-        # try:
-        #     # Get tf to end_eff_tag
-        #     self.ee_tag_2_panda_hand_tcp = self.tf_buffer.lookup_transform(
-        #         'panda_hand_tcp',
-        #         'panda_link0',
-        #         rclpy.time.Time())
+        try:
+            # Get tf to end_eff_tag
+            #self.get_logger().error("TEST 1!!!!")
+            self.panda_hand_tcp_2_panda_link0 = self.tf_buffer.lookup_transform(
+                'panda_link0',
+                'panda_hand_tcp',
+                rclpy.time.Time())
+    # TODO go through this mess and clean it up 
+    # TODO just make camera link the world frame so have a parent and child to camera link with no change
+    # TODO then get tf of robot base to world (should be from calibration step (double check its right))
+    # Overall itll be world as main parent with camera_link and panda_link0 as children and world will just be the same as camera
+            self.world_2_panda_link0.transform.translation.x = self.panda_hand_tcp_2_panda_link0.transform.translation.x
+            self.world_2_panda_link0.transform.translation.y = self.panda_hand_tcp_2_panda_link0.transform.translation.y
+            self.world_2_panda_link0.transform.translation.z = self.panda_hand_tcp_2_panda_link0.transform.translation.z
+            self.world_2_panda_link0.transform.rotation.x = self.panda_hand_tcp_2_panda_link0.transform.rotation.x
+            self.world_2_panda_link0.transform.rotation.y = self.panda_hand_tcp_2_panda_link0.transform.rotation.y
+            self.world_2_panda_link0.transform.rotation.z = self.panda_hand_tcp_2_panda_link0.transform.rotation.z
+            self.world_2_panda_link0.transform.rotation.w = self.panda_hand_tcp_2_panda_link0.transform.rotation.w
+        except TransformException:
+            self.get_logger().info("No tf")
 
-        #     # Average TF over a couple frames and save to file
-        #     self.get_logger().error(self.ee_tag_2_panda_hand_tcp)
-        # except:
-        #     # Get TF from file
-        #     self.get_logger().error("Not seeing calibrate")
-        #     pass
-        self.ee_tag_2_panda_hand_tcp.transform.translation.x = 0.0
-        self.ee_tag_2_panda_hand_tcp.transform.translation.y = 0.0
-        self.ee_tag_2_panda_hand_tcp.transform.translation.z = 0.0
+        self.broadcaster.sendTransform(self.world_2_panda_link0)
 
-        self.broadcaster.sendTransform(self.ee_tag_2_panda_hand_tcp)
+        self.world_2_cam.transform.translation.x = 0.0
+        self.world_2_cam.transform.translation.y = 0.0
+        self.world_2_cam.transform.translation.z = 0.0
+        self.world_2_cam.transform.rotation.x = 0.0
+        self.world_2_cam.transform.rotation.y = 0.0
+        self.world_2_cam.transform.rotation.z = 0.0
+        self.world_2_cam.transform.rotation.w = 1.0
+        self.broadcaster.sendTransform(self.world_2_cam)
 
     def timer_callback(self):
         """
