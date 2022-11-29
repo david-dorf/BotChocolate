e61f7a4875316681921d9fdb5aca76f3222b0362
Allan Garcia <allang@u.northwestern.edu>
2022-11-29 15:12:46 -0600

    Added scooper sub

--- moveBot/moveBot/trajectory.py
+++ moveBot/moveBot/trajectory.py
@@ -5,6 +5,8 @@ from rclpy.callback_groups import ReentrantCallbackGroup
 from std_srvs.srv import Empty
 import time
 from movebot_interfaces.srv import AddBox, GetPlanRqst
+from geometry_msgs.msg import Pose
+# from moveBot.gripper import Gripper
 
 class State(Enum):
     """Create a state machine to eventually implement planning the entire stored trajectory plan
@@ -19,18 +21,29 @@ class TrajectoryCaller(Node):
     def __init__(self):
         super().__init__("trajectory_node")
         self.cbgroup = ReentrantCallbackGroup()
+        self.scoop_sub =  self.create_subscription(Pose, 'scoop_pose', self.get_pose_callback, 10)
         self.plan_client = self.create_client(GetPlanRqst,"call_plan",callback_group=self.cbgroup)
         self.cart_client = self.create_client(GetPlanRqst,"call_cart",callback_group=self.cbgroup)
         self.execute_client = self.create_client(Empty,"call_execute",callback_group=self.cbgroup)
         self.request = GetPlanRqst.Request()
+        # self.gripper_command=Gripper()
         self.state = State.IDLE
+
+
+    def get_pose_callback(self, pose_msg):
+        """" Callback function of the turtle pose subscriber
+            Stores the TurtleSimPose message recieved 
+        """
+        self.pose=pose_msg 
+        print(self.pose)
+        
 
     def send_move_above_request(self):
         """Build the desired IkGoalRqstMsg to be sent over the client to make the robot plan and
         execute a trajectory. This request is the trajectory plan for moving above the object.
         """
         # self.request.start_pos.position and orientation already set as last position by the API
-        self.request.goal_pos.position = [0.5, 0.5, 0.4] # placeholder values, replace with CV
+        self.request.goal_pos.position = [self.pose.x,self.pose.y,self.pose.z] # placeholder values, replace with CV
         self.request.goal_pos.orientation = []
         self.request.is_xyzrpy = True
         self.request.execute_now = False
@@ -38,10 +51,11 @@ class TrajectoryCaller(Node):
         self.future = self.cart_client.call_async(self.request)
         rclpy.spin_until_future_complete(self, self.future)
         return self.future.result()
+     
 
     def send_move_down_request(self):
         """Generate the trajectory plan for moving down to eventually grip the object."""
-        self.request.goal_pos.position = [0.5, 0.5, 0.2] # placeholder values, replace with CV
+        self.request.goal_pos.position = [self.pose.x, self.pose.y, self.pose.z] # placeholder values, replace with CV
         self.request.goal_pos.orientation = []
         self.request.is_xyzrpy = True
         self.request.execute_now = False
@@ -114,24 +128,24 @@ class BoxCaller(Node):
 def main(args=None):
     rclpy.init(args=args)
 
-    box_client = BoxCaller()
-    box_client.add_box_request()
-    box_client.call_box_request()
+    # box_client = BoxCaller()
+    # box_client.add_box_request()
+    # box_client.call_box_request()
 
     trajectory_client = TrajectoryCaller()
     trajectory_client.send_move_above_request()
     trajectory_client.send_execute_request()
-    trajectory_client.send_move_down_request()
-    trajectory_client.send_execute_request()
-    # Add delay for gripper closing (WIP)
-    time.sleep(1)
-    trajectory_client.send_move_up_request()
-    trajectory_client.send_execute_request()
-    trajectory_client.send_move_home_request()
-    trajectory_client.send_execute_request()
+    # trajectory_client.send_move_down_request()
+    # trajectory_client.send_execute_request()
+    # # Add delay for gripper closing (WIP)
+    # time.sleep(1)
+    # trajectory_client.send_move_up_request()
+    # trajectory_client.send_execute_request()
+    # trajectory_client.send_move_home_request()
+    # trajectory_client.send_execute_request()
     trajectory_client.destroy_node()
 
-    box_client.clear_box_request()
+    # box_client.clear_box_request()
     rclpy.shutdown()
 
 if __name__ == '__main__':
